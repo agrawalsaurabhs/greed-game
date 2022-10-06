4f35d7fdcb051e84b824ce9c3e6edd92904fd13d
saurabh <saurabh.a@browserstack.com>
2022-10-07 00:21:55 +0530

    some more refactoring

--- game.rb
+++ game.rb
@@ -2,92 +2,105 @@ require_relative "dice_set"
 require_relative "player"
 
 class Game
-    
+    NUMBER_OF_DICE = 5
+	ELIGIBILITY_SCORE  = 300
+	MIN_WINNING_SCORE = 3000
+
     def initialize(num_players, dice_set)
         @players = []
-        @current_player = 1
-        @num_dice = 5
         @dice_set = dice_set
         for i in 1..num_players
             player = Player.new(i)
             @players.push(player)
         end
-        
-        # @players.each { |p| puts p.id } 
-        # puts @players.size
     end
 
-    def start_game
-        puts "game is getting, please wait for your turn"
-    end
+    # private
+    # def show_message(current_player, turn)
+    #     if(current_player==1)
+    #         puts "Turn #{turn}:"
+    #         puts "______"
+    #         turn +=1
+    #     end
+    # end
 
-    def play
+    public
+    def play_game
       
         last_round = false
         turn =1
         current_turn_score=0
-
+        current_player=1
+        
         while true do
-            #show the next turn only when current player =1
-            if(@current_player==1)
+            #show below message at the start of each turn i.e. when player 1's turn comes up
+            if(current_player==1)
                 puts "Turn #{turn}:"
                 puts "______"
                 turn +=1
             end
-
-            @dice_set.roll(@num_dice)
-            puts "Player #{@current_player} rolls: #{@dice_set.values.join(", ")}"
+            current_player_obj = @players.find { |p| p.id == current_player}
+            @dice_set.roll(NUMBER_OF_DICE)
+            puts "Player #{current_player} rolls: #{@dice_set.values.join(", ")}"
             current_turn_score +=  @dice_set.score(@dice_set.values)
             puts "Score in this round: #{current_turn_score}"
-
+            puts "Total score: #{current_player_obj.score + current_turn_score }"
             non_scoring_dice = @dice_set.non_scoring_dice
 
             loop do
                 if non_scoring_dice > 0
                     #give the player an option to roll again
+                    puts "Do you want to roll the non scoring #{non_scoring_dice} dice? (y/n)"
+                    continue_rolling = gets.chomp
                     
-                        puts "Do you want to roll the non scoring #{non_scoring_dice} dice? (y/n)"
-                        continue_rolling = gets.chomp
-                        #if player continues, roll the dice again
-                        if continue_rolling.downcase == "y"
-                            
-                            @dice_set.roll(non_scoring_dice)
-                            "Player #{@current_player} rolls: #{@dice_set.values.join(", ")}"
-                            
-                            current_roll_non_scoring_dice_count = @dice_set.non_scoring_dice
-                            non_scoring_dice =  current_roll_non_scoring_dice_count == non_scoring_dice ? 5 : current_roll_non_scoring_dice_count
-                            current_roll_score = @dice_set.score(@dice_set.values)
-                            current_turn_score = current_roll_score ==0 ? 0 : current_turn_score +=  current_roll_score
-                            continue_rolling = "n" if current_roll_score ==0
+                    if !(continue_rolling.downcase=="y" || continue_rolling.downcase=="n")
+                        begin
+                            raise ArgumentError, "invalid input,exiting!"
+                        rescue ArgumentError => e
+                            puts e.message
+                            exit
                         end
+                    end
+                    #if player continues, roll the dice again
+                    if continue_rolling == "y"
+                        
+                        @dice_set.roll(non_scoring_dice)
+                        puts "Player #{current_player} rolls: #{@dice_set.values.join(", ")}"
+                        
+                        current_roll_non_scoring_dice_count = @dice_set.non_scoring_dice
+                        non_scoring_dice =  current_roll_non_scoring_dice_count == non_scoring_dice ? 5 : current_roll_non_scoring_dice_count
+                        current_roll_score = @dice_set.score(@dice_set.values)
+                        puts "Score in this round: #{current_roll_score}"
+                        current_turn_score = current_roll_score ==0 ? 0 : current_turn_score +=  current_roll_score
+                        puts "Total score: #{current_player_obj.score + current_turn_score }"
+                        continue_rolling = "n" if current_roll_score ==0
+                    end
                 end
-            break if continue_rolling.downcase == "n" || non_scoring_dice==0 
+            break if continue_rolling == "n" || non_scoring_dice==0 
             end
 
             #if it's last round and last player has 
-            if last_round && @current_player == @players.size
+            if last_round && current_player == @players.size
                 break
             end
 
             #check for the last round 
-            if @current_player == @players.size && @players.find { |p| p.score >=500 } 
+            if current_player == @players.size && @players.find { |p| p.score >=500 } 
                 last_round = true
             end
 
             #move to the next player
             
              
-            current_player = @players.find { |p| p.id == @current_player}
+            current_player_obj = @players.find { |p| p.id == current_player}
 
-            if(current_player.score != 0 || current_turn_score>=300)
-                current_player.score+= current_turn_score
+            if(current_player_obj.score != 0 || current_turn_score>=300)
+                current_player_obj.score+= current_turn_score
             end
-            puts "Current Scoreboard"
-            @players.each { |p| puts p.score }
-            @current_player = @current_player + 1 <= @players.size ? @current_player + 1 : 1
-            @num_dice = 5
-            current_turn_score=0
             
+            current_player = current_player + 1 <= @players.size ? current_player + 1 : 1
+            current_turn_score=0
+            puts
         end
     end
 
@@ -116,7 +129,7 @@ end
 
 dice_set = DiceSet.new
 game = Game.new(num_players, dice_set)
-game.play
+game.play_game
 game.end_game
 
 
