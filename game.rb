a403e091e8743a3ecf6422276e49a3a781a67bc9
saurabh <saurabh.a@browserstack.com>
2022-10-06 18:55:42 +0530

    boilerplate code to start, stop and play the game
    boilerplate code to start, stop and play the game

--- game.rb
+++ game.rb
@@ -3,13 +3,41 @@ require_relative "player"
 
 class Game
     
-    def initialize(num_players)
+    def initialize(num_players, dice_set)
         @players = []
+        @current_player = 1
+        @num_dice = 5
+        @dice_set = dice_set
         for i in 1..num_players
             player = Player.new(i)
             @players.push(player)
         end
+        
         @players.each { |p| puts p.id } 
+        puts @players.size
+    end
+
+    def start_game
+        puts "game is getting, please wait for your turn"
+    end
+
+    def play
+        #roll the dice for next player
+        puts "rolling the dice for player #{@current_player}"
+        @dice_set.roll(@num_dice)
+        puts @dice_set.values
+
+
+        #set the next player 
+        @current_player = @current_player + 1 <= @players.size ? @current_player + 1 : 1
+        @num_dice = 5
+        
+
+    end
+
+    def end_game
+        @players.sort_by { |obj| obj.score}
+        puts "game is over, and the winner is #{@players[0]}"
     end
 end
 
@@ -19,7 +47,14 @@ begin
 rescue
     puts "please enter integer values greater than 1"
 end
-game = Game.new(num_players)
+dice_set = DiceSet.new
+game = Game.new(num_players, dice_set)
+game.play
+game.play
+game.play
+game.play
+game.play
+game.play
 
 
 
